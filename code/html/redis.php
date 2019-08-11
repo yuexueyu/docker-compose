c413078d584d5200715c9fe2ba7cb68960326395
Jack <jack@wu06.com>
2019-08-11 18:20:46 +0800

    finish

---
+++ code/html/redis.php
@@ -0,0 +1,16 @@
+﻿<?php
+$redis_host = "192.168.0.4";
+$redis_port = 6379;
+$user_pwd = "123456";
+$redis = new Redis();
+if ($redis->connect($redis_host, $redis_port) == false) {
+    die('111' . $redis->getLastError());
+}
+if ($redis->auth($user_pwd) == false) {
+    die('222' . $redis->getLastError());
+}
+if ($redis->set("welcome", "Hello Use Redis!") == false) {
+    die('333' . $redis->getLastError());
+}
+$value = $redis->get("welcome");
+echo $value;
